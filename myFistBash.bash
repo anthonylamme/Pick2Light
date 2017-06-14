e1e174d79ffa5753f43a7335205ec3ea7ce54e93
anthonylamme <anthonylamme@gmail.com>
2017-06-14 12:09:28 -0500

    Create myFistBash.bash

--- myFistBash.bash
+++ myFistBash.bash
@@ -1,7 +1,14 @@
 #!/bin/bash
-
+newHome=/home/pi/scripts
 clear
 
 echo "Starting Script"
 
 echo "Hi, $USER!"
+
+sudo apt-get -y install leafpad
+sudo apt-get -y install vim
+sudo apt-get -y install arduino
+sudo apt-get -y install ntpdate
+
+echo "Finished"
