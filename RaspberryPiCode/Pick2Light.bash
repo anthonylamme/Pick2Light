e431cda938ed6d47dc9a490179982c0787d1d577
anthonylamme <anthonylamme@gmail.com>
2017-07-26 17:51:58 -0500

    Add files via upload

--- RaspberryPiCode/Pick2Light.bash
+++ RaspberryPiCode/Pick2Light.bash
@@ -1,3 +1,3 @@
 #!/bin/bash -x
 
-sudo python /home/pi/scripts/main.py
+sudo python /home/pi/Scripts/Pick2Light/RaspberryPiCode/SLackBot/SlackBot.py  &  sudo python /home/pi/Scripts/Pick2Light/RaspberryPiCode/main.py
