c13d1b8240e740585e9966afd44cb983ad70e257
anthonylamme <anthonylamme@gmail.com>
2017-07-31 11:42:44 -0500

    Update Pick2Light.bash

--- RaspberryPiCode/Pick2Light.bash
+++ RaspberryPiCode/Pick2Light.bash
@@ -1,3 +1,5 @@
+# This script will allow for the SlackBot.py and the main.py to run in  parallel
+
 #!/bin/bash -x
 
 sudo python /home/pi/Scripts/Pick2Light/RaspberryPiCode/SLackBot/SlackBot.py  &  sudo python /home/pi/Scripts/Pick2Light/RaspberryPiCode/main.py
